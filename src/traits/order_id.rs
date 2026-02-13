87908a2e830504c7fbd626f638220fd13add2f64
WatchDG <baranov@w3m.co>
2026-02-13 13:49:42 +0300

    refactor: rename ReadOrderIdInner trait to ReadOrderIdInnerRef and update implementations for clarity

--- src/traits/order_id.rs
+++ src/traits/order_id.rs
@@ -1,35 +1,48 @@
 use crate::types::order_id::OrderId;
 
+pub trait ReadOrderIdInnerRef<OIDI> {
+    fn read_order_id_inner_ref(&self) -> &OIDI;
+}
+
 pub trait ReadOrderIdInner<OIDI> {
     fn read_order_id_inner(&self) -> OIDI;
+}
+
+impl<OIDI: Clone, T> ReadOrderIdInner<OIDI> for T
+where
+    T: ReadOrderIdInnerRef<OIDI>,
+{
+    fn read_order_id_inner(&self) -> OIDI {
+        self.read_order_id_inner_ref().clone()
+    }
 }
 
 pub trait ReadOrderId<OID> {
     fn read_order_id(&self) -> OrderId<OID>;
 }
 
-pub trait WriteOrderIdInner<OIDI> {
-    fn write_order_id_inner(&mut self, inner: OIDI) -> &mut Self;
-}
-
-pub trait WriteOrderId<OID> {
-    fn write_order_id(&mut self, id: &impl ReadOrderIdInner<OID>) -> &mut Self;
-}
-
-impl<OIDI, O> ReadOrderId<OIDI> for O
+impl<OIDI, T> ReadOrderId<OIDI> for T
 where
-    O: ReadOrderIdInner<OIDI>,
+    T: ReadOrderIdInner<OIDI>,
 {
     fn read_order_id(&self) -> OrderId<OIDI> {
         OrderId::new(self.read_order_id_inner())
     }
 }
 
-impl<OIDI: Clone, O> WriteOrderId<OIDI> for O
-where
-    O: WriteOrderIdInner<OIDI>,
-{
-    fn write_order_id(&mut self, id: &impl ReadOrderIdInner<OIDI>) -> &mut Self {
-        self.write_order_id_inner(id.read_order_id_inner())
-    }
-}
+// pub trait WriteOrderIdInner<OIDI> {
+//     fn write_order_id_inner(&mut self, inner: OIDI) -> &mut Self;
+// }
+
+// pub trait WriteOrderId<OID> {
+//     fn write_order_id(&mut self, id: &impl ReadOrderIdInner<OID>) -> &mut Self;
+// }
+
+// impl<OIDI: Clone, T> WriteOrderId<OIDI> for T
+// where
+//     T: WriteOrderIdInner<OIDI>,
+// {
+//     fn write_order_id(&mut self, id: &impl ReadOrderIdInner<OIDI>) -> &mut Self {
+//         self.write_order_id_inner(id.read_order_id_inner())
+//     }
+// }
--- src/types/order_id.rs
+++ src/types/order_id.rs
@@ -1,4 +1,4 @@
-use crate::traits::order_id::{ReadOrderIdInner, WriteOrderIdInner};
+use crate::traits::order_id::ReadOrderIdInnerRef;
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
 pub struct OrderId<OIDI>(OIDI);
@@ -9,15 +9,15 @@ impl<OIDI> OrderId<OIDI> {
     }
 }
 
-impl<OIDI: Clone> ReadOrderIdInner<OIDI> for OrderId<OIDI> {
-    fn read_order_id_inner(&self) -> OIDI {
-        self.0.clone()
+impl<OIDI> ReadOrderIdInnerRef<OIDI> for OrderId<OIDI> {
+    fn read_order_id_inner_ref(&self) -> &OIDI {
+        &self.0
     }
 }
 
-impl<OIDI> WriteOrderIdInner<OIDI> for OrderId<OIDI> {
-    fn write_order_id_inner(&mut self, inner: OIDI) -> &mut Self {
-        self.0 = inner;
-        self
-    }
-}
+// impl<OIDI> WriteOrderIdInner<OIDI> for OrderId<OIDI> {
+//     fn write_order_id_inner(&mut self, inner: OIDI) -> &mut Self {
+//         self.0 = inner;
+//         self
+//     }
+// }
