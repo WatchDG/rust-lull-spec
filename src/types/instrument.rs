41ca76075b4ceb611248c1d3699dcee3cb7f6c3c
WatchDG <baranov@w3m.co>
2026-02-15 02:39:57 +0300

    feat: extend Instrument struct to include LotSize, enhancing instrument representation with lot size management

--- src/types/instrument.rs
+++ src/types/instrument.rs
@@ -1,6 +1,8 @@
 use crate::types::instrument_id::InstrumentId;
+use crate::types::lot_size::LotSize;
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
-pub struct Instrument<II> {
+pub struct Instrument<II, LSI> {
     pub id: InstrumentId<II>,
+    pub lot_size: LotSize<LSI>,
 }
